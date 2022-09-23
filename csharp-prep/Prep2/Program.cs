c5fea396a89d5396c97edecc8d999f0685cffa75
kigamekisia <kigamekisia@gmail.com>
2022-09-23 19:01:30 +0300

    update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,6 +7,39 @@ namespace Prep2
         static void Main(string[] args)
         {
             Console.WriteLine("This is Prep 2");
+            Console.Write("What is your grade percentage? ");
+            string userGrade = Console.ReadLine();
+            int number = int.Parse(userGrade);
+            string letter ="";
+            if (number >= 90)
+            {
+                letter ="A";
+            }
+            else if (number >= 80)
+            {
+                letter ="B";
+            }
+            else if (number >= 70)
+            {
+                letter ="C";
+            }
+            else if (number >= 60)
+            {
+                letter ="D";
+            }
+            else
+            {
+                letter ="F";
+            }
+            Console.WriteLine($"Your garde is {letter}");
+            if (number >= 70)
+            {
+                Console.WriteLine("Congratulations you passed the class");
+            }
+            else
+            {
+                Console.WriteLine("You failed the class, Work harder next time");
+            }
         }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,7 +7,38 @@ namespace Prep4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 4");
+            List <int> numbers = new List<int>();
+            int userNumber=-1;
+            while(userNumber!=0)
+            {
+                Console.Write("Enter a list of numbers (type 0 when finished): ");
+                string userResponse = Console.ReadLine();
+                userNumber = int.Parse(userResponse);
+                numbers.Add(userNumber);
+            }
+            // part 1: compute the sum
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum+=number;
+            }
+            Console.WriteLine($"The sum is {sum}");
+            // Part 2 compute the average
+            float average = sum / numbers.Count;
+            Console.WriteLine($"The average is {average}");
+            // Part 3 find the max
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number>max)
+                {
+                    {
+                        // if this number is greater that the max then we have found the new max
+                        max= number;
+                    }
+                }
+            }
+            Console.WriteLine($"The Max is: {max}");
         }
     }
 }
